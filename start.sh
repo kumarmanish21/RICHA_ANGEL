21ddc44c59b7ea5bcf28c0f89983c5d3ef470429
mannuXD <90824516+kumarmanish21@users.noreply.github.com>
2023-06-17 13:15:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,9 +7,6 @@ else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
 fi
-  echo "Cloning $BRANCH branch...."
-  git clone https://github.com/nodejs/node /VCPlayerBot
-fii 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
