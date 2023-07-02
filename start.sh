832df6db0aa8026454665bc11f671960c7c7b2a6
mannuXD <90824516+kumarmanish21@users.noreply.github.com>
2023-07-02 22:25:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/kumarmanish21/RICHA_ANGEL /RICHA_ANGEL
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/kumarmanish21/RICHA_ANGEL -b $BRANCH /RICHA_ANGEL
 fi
-cd /VCPlayerBot
+cd /RICHA_ANGEL
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
